ba037e3a70f560584d5168a49e0d49f5202b73b8
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-02-11 15:35:45 -0300

    definido as cores

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,29 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    return () => {};
+    return () => { };
   }, []);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-hero">
       <div className="mx-auto">
-        <div className="container bg-white p-8 border border-yellow-50 text-center">
-          <h1 className="text-4xl font-bold mb-2">Mirelle </h1>
-          <h1 className="text-4xl font-bold mb-2">& </h1>
-          <h1 className="text-4xl font-bold mb-2">Rafael </h1>
+        <div className="bg-white p-6 border-4 border-blue-500 text-center rounded-xl relative">
+          <img
+            src="/img/logo.png"
+            alt="Logo"
+            className="mx-auto mb-2"
+            style={{ width: '90px', height: '90px' }}
+          />
+          <h1 className="text-4xl text-black font-bold mb-2">Mirelle </h1>
+          <h1 className="text-4xl text-black font-bold mb-2">& </h1>
+          <h1 className="text-4xl text-black font-bold mb-2">Rafael </h1>
           <h2 className="text-2xl mb-2">24/08/24</h2>
-          <p className="mb-4">Bem-vindo ao nosso site de casamento! Estamos muito felizes <br/> e contamos com a presença de todos no nosso grande dia!</p>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Te Amo Muito Amor
+          <p className="mb-4 text-black">Bem-vindo ao nosso site de casamento! Estamos muito felizes <br /> e queremos compartilhar nossa alegria com vocês</p>
+          <button className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-900 text-white font-bold py-2 px-4 rounded">
+            Confirmar Presença  
           </button>
         </div>
-      </div>  
+      </div>
     </div>
   );
 }
