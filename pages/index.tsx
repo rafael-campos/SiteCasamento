aa5610965ebf39e022a8368c0d752032d149a467
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-02-11 12:11:12 -0300

    estrutura inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Typography, Button } from 'antd';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+const { Title, Text } = Typography;
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  useEffect(() => {
+    // Código do lado do cliente aqui, se necessário
+
+    return () => {
+      // Limpeza, se necessário
+    };
+  }, []);
+
   return (
-  <div>
-    ola
-  </div>
+    <div className=" flex items-center justify-center min-h-screen ">
+      <div className="mx-auto">
+        <div className="container bg-white p-8 border border-yellow-50 text-center">
+          <Title level={1}>Mirelle</Title>
+          <Title level={2}>Rafael</Title>
+          <Text>24/08/24</Text>
+        </div>
+      </div>
+
+
+
+    
+    </div>
   );
 }
