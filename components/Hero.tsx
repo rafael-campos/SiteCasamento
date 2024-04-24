419d0ff3dcd4651c8063a1087dc8510cfe6f73e1
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-04-23 23:28:14 -0300

    hero

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <div className="bg-[url('/image/capa.jpg')] w-full h-[100vh] bg-cover">
+      <div className="flex flex-col items-center justify-center pt-32 pb-10 px-[12%]">
+        <div className="text-center">
+        <p className="text-[#b0cddf] font-alumni text-6xl font-bold">Rafael & Mirelle</p>
+          <p className="text-black font-andika text-justify mt-4">
+            Junte-se a nós para celebrar o nosso casamento!
+          </p>
+          <p className="text-black font-andika text-justify mt-2">
+            Data: 24 de agosto de 2024
+          </p>
+          <div className="mt-8">
+            <button className="inline-block bg-serenity text-white mt-3 py-1 px-6 rounded-lg font-medium text-lg cursor-pointer shadow-md hover:bg-serenity-dark transition-colors">
+              Confirmar Presença
+            </button>
+          </div>
+        </div>
+        <div className="mt-8">
+          <Image src='/image/capa.jpg' width={500} height={200} alt="Imagem do casal" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Hero from '@/components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <div>
-    ola
-   </div>
-  )
+    <div>
+
+      <Hero />
+ 
+    </div>
+  );
 }
