5411e49ced16b1fbed174cd7b0449ce1b489284f
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-05-31 08:36:51 -0300

    arrumado navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,82 @@
-// Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { FaHeart, FaInfo, FaImages, FaGift, FaEnvelopeOpenText } from 'react-icons/fa';
+import {
+  HeartFilled,
+  InfoCircleOutlined,
+  PictureOutlined,
+  GiftOutlined,
+  MailOutlined,
+  MenuOutlined
+} from '@ant-design/icons';
+import { Layout, Menu, Drawer } from 'antd';
+
+const { Header } = Layout;
 
 const Navbar: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const handleLinkClick = () => {
+    setVisible(false); // Fecha o Drawer ao clicar em um link em dispositivos móveis
+  };
+
   return (
-    <nav className="flex items-center justify-between px-10 py-5 shadow-md w-full fixed top-0 z-50 bg-white/90 backdrop-blur-md">
-      <Link href="/#inicio" scroll={true}>
-        <div className="text-2xl font-bold text-blue-400 hover:text-blue-500 transition-all duration-300 cursor-pointer flex items-center">
-          <FaHeart className="mr-2" /> Rafael & Mirelle
+    <Layout>
+      <Header className="flex items-center justify-between px-4 py-3 shadow-md w-full fixed top-0 z-50 bg-white/90 backdrop-blur-md">
+        <Link href="/#inicio" scroll={true}>
+          <div className="text-2xl font-bold text-blue-400 hover:text-blue-500 transition-all duration-300 cursor-pointer flex items-center">
+            <HeartFilled style={{ color: '#FF0000' }} className="mr-2" /> Rafael & Mirelle
+          </div>
+        </Link>
+        <div className="hidden md:flex space-x-4">
+          <Link href="/#informacoes" scroll={true}>
+            <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
+              <InfoCircleOutlined className="mr-2" /> Informações
+            </div>
+          </Link>
+          <Link href="/#galeria" scroll={true}>
+            <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
+              <PictureOutlined className="mr-2" /> Galeria
+            </div>
+          </Link>
+          <Link href="/#lista-de-presentes" scroll={true}>
+            <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
+              <GiftOutlined className="mr-2" /> Lista de Presentes
+            </div>
+          </Link>
+    
         </div>
-      </Link>
-      <div className="flex space-x-4">
-        <Link href="/#informacoes" scroll={true}>
-          <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
-            <FaInfo className="mr-2" /> Informações
-          </div>
-        </Link>
-        <Link href="/#galeria" scroll={true}>
-          <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
-            <FaImages className="mr-2" /> Galeria
-          </div>
-        </Link>
-        <Link href="/#lista-de-presentes" scroll={true}>
-          <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
-            <FaGift className="mr-2" /> Lista de Presentes
-          </div>
-        </Link>
-        <Link href="/#rsvp" scroll={true}>
-          <div className="text-gray-600 hover:text-gray-800 transition-all duration-300 cursor-pointer flex items-center">
-            <FaEnvelopeOpenText className="mr-2" /> RSVP
-          </div>
-        </Link>
-      </div>
-    </nav>
+        <MenuOutlined className="md:hidden text-2xl" onClick={showDrawer} />
+      </Header>
+      <Drawer title="Menu" placement="right" onClose={onClose} visible={visible}>
+        <Menu mode="inline" onClick={handleLinkClick}> 
+          {/* Adiciona o onClick aqui para fechar o Drawer ao clicar no link */}
+          <Menu.Item key="1">
+            <Link href="/#informacoes" scroll={true}>
+               Informações 
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link href="/#galeria" scroll={true}>
+              Galeria
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link href="/#lista-de-presentes" scroll={true}>
+              Lista de Presentes
+            </Link>
+          </Menu.Item>
+  
+        </Menu>
+      </Drawer>
+    </Layout>
   );
 };
 
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -13,9 +13,10 @@ interface Presente {
     preco: string;
     codigoPix: string;
     linkProduto: string; // Adicionado link do produto para cada presente
+    linkPagamentoCartao: string; // Adiciona a variável de link de pagamento
 }
 
-// Dados fictícios dos presentes
+// Dados fictícios dos presentes (Agora com o link de pagamento)
 const presentes: Presente[] = [
     // ... seus outros presentes ...
     {
@@ -25,7 +26,8 @@ const presentes: Presente[] = [
         descricao: 'Pra ajudar agente a nao agarrar comida',
         preco: 'R$ 172,00',
         codigoPix: '00020126730014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250211Frigideira 5204000053039865406172.005802BR5922Rafael Henrique Campos6008Brasilia62090505xpjkq630472B3x',
-        linkProduto: 'https://www.magazineluiza.com.br/frigideira-antiaderente-ceramica-cobre-com-tampa-maxchef/p/fdhc2g5406/ud/udfg/?partner_id=64068&utm_source=pdp&utm_medi'
+        linkProduto: 'https://www.magazineluiza.com.br/frigideira-antiaderente-ceramica-cobre-com-tampa-maxchef/p/fdhc2g5406/ud/udfg/?partner_id=64068&utm_source=pdp&utm_medi',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/frigideira' // Adicione o link real de pagamento aqui
     },
     {
         id: '2',
@@ -34,7 +36,8 @@ const presentes: Presente[] = [
         descricao: 'Presenteie com esse aspirador de pó',
         preco: 'R$ 142,49',
         codigoPix: '00020126930014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250231Estou enviado o aspirador dr po5204000053039865406142.495802BR5922Rafael Henrique Campos6008Brasilia62090505m1kef63040A10',
-        linkProduto: 'https://www.pontofrio.com.br/aspirador-de-po-vertical-2-em-1-mondial-turbo-cycle-ap36-1100w-vermelho-15003643/p/15003651?utm_medium=cpc&utm_source=GP_PLA&IdSku=15003651&idLojista=16&tipoLojista=1P&gclsrc=aw.ds&&utm_campaign=gg_pmax_core_elpo&gad_source=1&gclid=CjwKCAjw3NyxBhBmEiwAyofDYS3_j4kyi1CPg6ENCUSRKW4bovK4nlMff1NVOYGGPg_uA6J5w9qkVBoCr5AQAvD_BwE'
+        linkProduto: 'https://www.pontofrio.com.br/aspirador-de-po-vertical-2-em-1-mondial-turbo-cycle-ap36-1100w-vermelho-15003643/p/15003651?utm_medium=cpc&utm_source=GP_PLA&IdSku=15003651&idLojista=16&tipoLojista=1P&gclsrc=aw.ds&&utm_campaign=gg_pmax_core_elpo&gad_source=1&gclid=CjwKCAjw3NyxBhBmEiwAyofDYS3_j4kyi1CPg6ENCUSRKW4bovK4nlMff1NVOYGGPg_uA6J5w9qkVBoCr5AQAvD_BwE',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/aspirador' // Adicione o link real de pagamento aqui
     },
     {
         id: '3',
@@ -43,7 +46,8 @@ const presentes: Presente[] = [
         descricao: 'Presenteie com essa sanduicheira ',
         preco: 'R$ 129,00',
         codigoPix: '00020126810014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250219Enviando a misteira5204000053039865406129.005802BR5922Rafael Henrique Campos6008Brasilia62090505siowv63043199',
-        linkProduto: 'https://www.amazon.com.br/Brit%C3%A2nia-SANDUICHEIRA-GRILL-PRESS-BGR27I/dp/B09WWY48B7/ref=asc_df_B09WWY48B7/?tag=googleshopp00-20&linkCode=df0&hvadid=555495720867&hvpos=&hvnetw=g&hvrand=5768162328902286780&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001585&hvtargid=pla-1689628867581&psc=1&mcid=6811c2f452653d3781966c110612a833'
+        linkProduto: 'https://www.amazon.com.br/Brit%C3%A2nia-SANDUICHEIRA-GRILL-PRESS-BGR27I/dp/B09WWY48B7/ref=asc_df_B09WWY48B7/?tag=googleshopp00-20&linkCode=df0&hvadid=555495720867&hvpos=&hvnetw=g&hvrand=5768162328902286780&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001585&hvtargid=pla-1689628867581&psc=1&mcid=6811c2f452653d3781966c110612a833',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/sanduicheira' // Adicione o link real de pagamento aqui
     },
     {
         id: '4',
@@ -52,7 +56,8 @@ const presentes: Presente[] = [
         descricao: 'Presenteie com essa airflyer',
         preco: 'R$ 269,00',
         codigoPix: '00020126830014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250221Enviando ima airflyer5204000053039865406269.005802BR5922Rafael Henrique Campos6008Brasilia62090505pa23r63045BCA',
-        linkProduto: 'https://www.casasbahia.com.br/fritadeira-eletrica-sem-oleo-air-fryer-mondial-af-30-family-inox-iv-35l-preta-55010765/p/55010765?utm_medium=cpc&utm_source=GP_PLA&IdSku=55010765&idLojista=10037&tipoLojista=1P&gclsrc=aw.ds&&utm_campaign=gg_pmax_core_elpo_apostas&gad_source=4&gclid=CjwKCAjw3NyxBhBmEiwAyofDYc1oTAYDz3eS5d7OVFvhCrpWdr_cdtn25CTXPmbNMS59WHtI0_L5TxoCUVQQAvD_BwE'
+        linkProduto: 'https://www.casasbahia.com.br/fritadeira-eletrica-sem-oleo-air-fryer-mondial-af-30-family-inox-iv-35l-preta-55010765/p/55010765?utm_medium=cpc&utm_source=GP_PLA&IdSku=55010765&idLojista=10037&tipoLojista=1P&gclsrc=aw.ds&&utm_campaign=gg_pmax_core_elpo_apostas&gad_source=4&gclid=CjwKCAjw3NyxBhBmEiwAyofDYc1oTAYDz3eS5d7OVFvhCrpWdr_cdtn25CTXPmbNMS59WHtI0_L5TxoCUVQQAvD_BwE',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/airflyer' // Adicione o link real de pagamento aqui
     },
     {
         id: '5',
@@ -61,7 +66,8 @@ const presentes: Presente[] = [
         descricao: 'Para brindar com os noivos',
         preco: 'R$ 59,90',
         codigoPix: '00020126670014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250205copos520400005303986540559.905802BR5922Rafael Henrique Campos6008Brasilia620905052t6nj6304C1BD',
-        linkProduto: 'https://www.amazon.com.br/dp/B0CPM571Y5?ref=cm_sw_r_apan_dp_F63YR1M1H1DVTKAK01M2&ref_=cm_sw_r_apan_dp_F63YR1M1H1DVTKAK01M2&language=pt-BR'
+        linkProduto: 'https://www.amazon.com.br/dp/B0CPM571Y5?ref=cm_sw_r_apan_dp_F63YR1M1H1DVTKAK01M2&ref_=cm_sw_r_apan_dp_F63YR1M1H1DVTKAK01M2&language=pt-BR',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/copo' // Adicione o link real de pagamento aqui
     },
     {
         id: '6',
@@ -70,7 +76,8 @@ const presentes: Presente[] = [
         descricao: 'Para os noivos guardarem salada',
         preco: 'R$ 172,00',
         codigoPix: '00020126670014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250205Potes5204000053039865406172.005802BR5922Rafael Henrique Campos6008Brasilia620905058di5p630494CE',
-        linkProduto: 'https://www.amazon.com.br/dp/B07QJXQHQ3?ref=cm_sw_r_apan_dp_PZ7BZ7Y28B4ZD37W0TV6&ref_=cm_sw_r_apan_dp_PZ7BZ7Y28B4ZD37W0TV6&language=pt-BR'
+        linkProduto: 'https://www.amazon.com.br/dp/B07QJXQHQ3?ref=cm_sw_r_apan_dp_PZ7BZ7Y28B4ZD37W0TV6&ref_=cm_sw_r_apan_dp_PZ7BZ7Y28B4ZD37W0TV6&language=pt-BR',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/pote' // Adicione o link real de pagamento aqui
     },
     {
         id: '7',
@@ -79,7 +86,8 @@ const presentes: Presente[] = [
         descricao: 'Panela',
         preco: 'R$ 78,99',
         codigoPix: '00020126730014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250211Panela 3em1520400005303986540578.995802BR5922Rafael Henrique Campos6008Brasilia62090505l3xi96304D457',
-        linkProduto: 'https://www.amazon.com.br/dp/B0BZ1RJNJ2?ref=cm_sw_r_apan_dp_WQJRGKDFE9Y0GDWMKYEG&ref_=cm_sw_r_apan_dp_WQJRGKDFE9Y0GDWMKYEG&language=pt-BR&th=1'
+        linkProduto: 'https://www.amazon.com.br/dp/B0BZ1RJNJ2?ref=cm_sw_r_apan_dp_WQJRGKDFE9Y0GDWMKYEG&ref_=cm_sw_r_apan_dp_WQJRGKDFE9Y0GDWMKYEG&language=pt-BR&th=1',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/panela' // Adicione o link real de pagamento aqui
     },
     {
         id: '8',
@@ -88,7 +96,8 @@ const presentes: Presente[] = [
         descricao: 'Multiprocessador ',
         preco: 'R$ 145,12',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.amazon.com.br/dp/B0876XRYGT?ref=cm_sw_r_apan_dp_4Y5NADY0Y0TBVF58M3WH&ref_=cm_sw_r_apan_dp_4Y5NADY0Y0TBVF58M3WH&language=pt-BR'
+        linkProduto: 'https://www.amazon.com.br/dp/B0876XRYGT?ref=cm_sw_r_apan_dp_4Y5NADY0Y0TBVF58M3WH&ref_=cm_sw_r_apan_dp_4Y5NADY0Y0TBVF58M3WH&language=pt-BR',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/multiprocessador' // Adicione o link real de pagamento aqui
     },
     {
         id: '9',
@@ -97,7 +106,8 @@ const presentes: Presente[] = [
         descricao: 'Multiprocessador ',
         preco: 'R$ 134,99',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.casasbahia.com.br/conjunto-de-travessas-refratarias-marinex-em-vidro---10-pecas-55058982.html'
+        linkProduto: 'https://www.casasbahia.com.br/conjunto-de-travessas-refratarias-marinex-em-vidro---10-pecas-55058982.html',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/travessa' // Adicione o link real de pagamento aqui
     },
     {
         id: '10',
@@ -106,7 +116,8 @@ const presentes: Presente[] = [
         descricao: 'Panela de Pressão Elétrica 5 Litros ',
         preco: 'R$ 365,66',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.casaevideo.com.br/panela-de-pressao-eletrica-5l-mondial-pe38-preto-com-prata-127v/p'
+        linkProduto: 'https://www.casaevideo.com.br/panela-de-pressao-eletrica-5l-mondial-pe38-preto-com-prata-127v/p',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/pressao' // Adicione o link real de pagamento aqui
     },
     {
         id: '11',
@@ -115,7 +126,8 @@ const presentes: Presente[] = [
         descricao: 'Panela Elétrica de Arroz Mondial PE-43 6 Xícaras - Preta/Inox ',
         preco: 'R$ 189,90',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.pontofrio.com.br/panela-eletrica-de-arroz-mondial-pe-43-6-xicaras-preta-inox/p/55011884'
+        linkProduto: 'https://www.pontofrio.com.br/panela-eletrica-de-arroz-mondial-pe-43-6-xicaras-preta-inox/p/55011884',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/arroz' // Adicione o link real de pagamento aqui
     },
     {
         id: '12',
@@ -124,7 +136,8 @@ const presentes: Presente[] = [
         descricao: 'Jogo Panelas 5 Peças Sartin Champanhe com Frigideira Antiaderente - Mimo Style ',
         preco: 'R$ 394,20',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.magazineluiza.com.br/jogo-panelas-5-pecas-sartin-champanhe-com-frigideira-antiaderente-mimo-style/p/jjbk6g7073/ud/cjpn/?partner_id=64068&utm_source=pdp&utm_medium=share'
+        linkProduto: 'https://www.magazineluiza.com.br/jogo-panelas-5-pecas-sartin-champanhe-com-frigideira-antiaderente-mimo-style/p/jjbk6g7073/ud/cjpn/?partner_id=64068&utm_source=pdp&utm_medium=share',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/kitpanela' // Adicione o link real de pagamento aqui
     },
     {
         id: '13',
@@ -133,7 +146,8 @@ const presentes: Presente[] = [
         descricao: 'Kit Churrasco Ou Cozinha 25 Peças Com Maleta Em Couro ',
         preco: 'R$ 193,00',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://produto.mercadolivre.com.br/MLB-1251203109-kit-churrasco-ou-cozinha-25-pecas-com-maleta-em-couro-_JM?attributes=COLOR_SECONDARY_COLOR:QmxhY2s='
+        linkProduto: 'https://produto.mercadolivre.com.br/MLB-1251203109-kit-churrasco-ou-cozinha-25-pecas-com-maleta-em-couro-_JM?attributes=COLOR_SECONDARY_COLOR:QmxhY2s=',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/kitchurrasco' // Adicione o link real de pagamento aqui
     },
     {
         id: '14',
@@ -142,7 +156,8 @@ const presentes: Presente[] = [
         descricao: 'Jogo Talheres Aço Inox Faqueiro Malibu 91 Peças Tramontina ',
         preco: 'R$ 284,05',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://produto.mercadolivre.com.br/MLB-3662020297-jogo-talheres-aco-inox-faqueiro-malibu-91-pecas-tramontina-_JM?attributes=COLOR_SECONDARY_COLOR:UHJhdGVhZG8='
+        linkProduto: 'https://produto.mercadolivre.com.br/MLB-3662020297-jogo-talheres-aco-inox-faqueiro-malibu-91-pecas-tramontina-_JM?attributes=COLOR_SECONDARY_COLOR:UHJhdGVhZG8=',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/jogotalheres' // Adicione o link real de pagamento aqui
     },
     {
         id: '15',
@@ -151,7 +166,8 @@ const presentes: Presente[] = [
         descricao: 'Aparelho Jantar e Chá 30 Pçs Biona Donna Cena Inglesa',
         preco: 'R$ 382,90',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.amazon.com.br/dp/B09RGTPPT7?ref=cm_sw_r_apan_dp_D6A6Z8WH3S9VRBSSAWF7&ref_=cm_sw_r_apan_dp_D6A6Z8WH3S9VRBSSAWF7&language=pt-BR'
+        linkProduto: 'https://www.amazon.com.br/dp/B09RGTPPT7?ref=cm_sw_r_apan_dp_D6A6Z8WH3S9VRBSSAWF7&ref_=cm_sw_r_apan_dp_D6A6Z8WH3S9VRBSSAWF7&language=pt-BR',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/jantar' // Adicione o link real de pagamento aqui
     },
     {
         id: '16',
@@ -160,7 +176,8 @@ const presentes: Presente[] = [
         descricao: 'Batedeira Planetária Britânia BBPE01 12 Velocidades 4L 500W 110v',
         preco: 'R$ 279,90',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.amazon.com.br/Batedeira-Planet%C3%A1ria-Brit%C3%A2nia-BBPE01-Velocidades/dp/B0CN1PFKTL/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=9PYUI8XG8HI7&dib=eyJ2IjoiMSJ9.5rQ1kTa_pTQj8qDG6r8etDrMexbGMHRc615eu2t3QFp7b77u8TR0W9kcs-6-sENsAaB2qFLc-zrVzlLYQn9Z5MtX7rP0QiB17JleYTSK2FSf4lcGY0UrmdD0UMexjwCEXxErFXd4qZxl35jQ_u8uYK1XtkATRIk663EiYYXPX3b8Fcg7yk8FiUVszR678VjSMl9WRIX1UFjuKd_b_GM-aNVgY1LATWedzj3wQ5fXGiw.bLOYfgh_G7p0a4Li2n0-EewEXKQYg9XMefXjOtGYx6M&dib_tag=se&keywords=Batedeira+Planet%C3%A1ria+Brit%C3%A2nia+BBPE01+12+Velocidades+4L+500W+110V&qid=1717087488&s=kitchen&sprefix=batedeira+planet%C3%A1ria+brit%C3%A2nia+bbpe01+12+velocidades+4l+500w+110v%2Ckitchen%2C281&sr=1-3&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e'
+        linkProduto: 'https://www.amazon.com.br/Batedeira-Planet%C3%A1ria-Brit%C3%A2nia-BBPE01-Velocidades/dp/B0CN1PFKTL/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=9PYUI8XG8HI7&dib=eyJ2IjoiMSJ9.5rQ1kTa_pTQj8qDG6r8etDrMexbGMHRc615eu2t3QFp7b77u8TR0W9kcs-6-sENsAaB2qFLc-zrVzlLYQn9Z5MtX7rP0QiB17JleYTSK2FSf4lcGY0UrmdD0UMexjwCEXxErFXd4qZxl35jQ_u8uYK1XtkATRIk663EiYYXPX3b8Fcg7yk8FiUVszR678VjSMl9WRIX1UFjuKd_b_GM-aNVgY1LATWedzj3wQ5fXGiw.bLOYfgh_G7p0a4Li2n0-EewEXKQYg9XMefXjOtGYx6M&dib_tag=se&keywords=Batedeira+Planet%C3%A1ria+Brit%C3%A2nia+BBPE01+12+Velocidades+4L+500W+110V&qid=1717087488&s=kitchen&sprefix=batedeira+planet%C3%A1ria+brit%C3%A2nia+bbpe01+12+velocidades+4l+500w+110v%2Ckitchen%2C281&sr=1-3&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/batedeira' // Adicione o link real de pagamento aqui
     },
     {
         id: '17',
@@ -169,7 +186,8 @@ const presentes: Presente[] = [
         descricao: 'Mixer 3 em 1, 400W, 2 velocidades, Bmx400, Preto, 110v, Britânia',
         preco: 'R$ 199,00',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.amazon.com.br/dp/B08YHR9C4S?ref=cm_sw_r_apan_dp_DF9TQMEFGR2PVV7DN8VD&ref_=cm_sw_r_apan_dp_DF9TQMEFGR2PVV7DN8VD&language=pt-BR&th=1'
+        linkProduto: 'https://www.amazon.com.br/dp/B08YHR9C4S?ref=cm_sw_r_apan_dp_DF9TQMEFGR2PVV7DN8VD&ref_=cm_sw_r_apan_dp_DF9TQMEFGR2PVV7DN8VD&language=pt-BR&th=1',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/mixer' // Adicione o link real de pagamento aqui
     },
     {
         id: '18',
@@ -178,7 +196,8 @@ const presentes: Presente[] = [
         descricao: 'Organizador de geladeira',
         preco: 'R$ 114,99',
         codigoPix: '00020126780014br.gov.bcb.pix013692fb26d6-62d7-40fc-a775-c6ba8f2ae2250216Multiprocessador5204000053039865406145.125802BR5922Rafael Henrique Campos6008Brasilia62090505zhdqr6304EA04',
-        linkProduto: 'https://www.amazon.com.br/dp/B0CCLM43XP?ref=cm_sw_r_apan_dp_C7DS3PXQTDA2DTMJFYR2&ref_=cm_sw_r_apan_dp_C7DS3PXQTDA2DTMJFYR2&language=pt-BR'
+        linkProduto: 'https://www.amazon.com.br/dp/B0CCLM43XP?ref=cm_sw_r_apan_dp_C7DS3PXQTDA2DTMJFYR2&ref_=cm_sw_r_apan_dp_C7DS3PXQTDA2DTMJFYR2&language=pt-BR',
+        linkPagamentoCartao: 'https://www.exemplo.com/pagamento/organizador' // Adicione o link real de pagamento aqui
     },
     // ... seus outros presentes ...
 ];
@@ -247,6 +266,9 @@ const PresentePage: React.FC = () => {
                             <button onClick={copyPixCode} className="bg-gradient-to-r from-blue-500 to-blue-600 text-white py-1 px-8 rounded-lg font-medium text-lg cursor-pointer shadow-md hover:from-blue-600 hover:to-blue-700 transition-colors flex items-center justify-center">
                                 <FaCopy className="mr-2" />
                                 {copied ? 'Copiado!' : 'Copiar Pix'}
+                            </button>
+                            <button onClick={() => openLinkInNewTab(presente.linkPagamentoCartao)} className="bg-gradient-to-r from-blue-500 to-blue-600 text-white py-1 px-8 rounded-lg font-medium text-lg cursor-pointer shadow-md hover:from-blue-600 hover:to-blue-700 transition-colors flex items-center justify-center mt-4">
+                                Pagar com Cartão
                             </button>
                         </div>
                     </div>
