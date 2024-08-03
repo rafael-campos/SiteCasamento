62de26a09de54ea29454ff071e18e360a7396936
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2024-08-03 12:53:07 -0300

    confirmacao 2

---
+++ pages/confirmados.tsx
@@ -0,0 +1,11 @@
+import ConfirmarPresenca from "@/components/ConfirmarPresenca";
+import Navbar from "@/components/Navbar";
+
+export default function Presenca(){
+    return(
+        <div>
+            <Navbar/>
+            <ConfirmarPresenca/>
+        </div>
+    )
+}
